dfed09211c22d5a471c4191980f958c2b3508205
”18-Nagata” <tomoyarenofa@gmail.com>
2019-07-30 04:40:58 +0000

    kadai11

--- practice.php
+++ practice.php
@@ -5,7 +5,7 @@ echo $a + $b;
 $array_month = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
 echo $array_month[7];
 $hello ="Hello!";
-$name = "永田智耶";
+$name = "Nagata";
 $world = "'sWorld";
 echo $hello . $name . $world;
 $tech_boost = "tech";
@@ -27,4 +27,4 @@ $_2018_calendar = [
 ];
 
 // 12月を表示する
-echo $_2018_calendar[December];
+echo $_2018_calendar["December"];
