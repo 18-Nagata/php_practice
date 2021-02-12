5753abcf117b8d5886c82cdc10f74c413c18f260
tomoya.nagata2 <tomoyarenofa@gmail.com>
2021-02-12 07:48:19 +0000

    kadai13

--- practice12.php
+++ practice12.php
@@ -1,30 +1,36 @@
 <?php
-$name= "Nagata";
-if ($name == "Nagata") {
-    echo '私はNagataです。';
-}else {
-    echo '私はNagataではありません。';
+$name = "nkgata";
+
+if($name == "nagata") {
+    echo '私はnagataです。';
+} else {
+    echo '私はnagataではありません。';
 }
+
 $total = 0;
-for ($i = 0; $i <= 10000; $i++) {
-    $total += $i;
+for($i = 1; $i <= 10000; $i++) {
+     $total += $i;
 }
 echo $total;
-$fruits = array("apple", "orange", "banana", "strawberry", "grape");
-foreach ($fruits as $fruit) {
-    echo "要素は" . $fruits;
+
+$fruits = array("apple", "orange", "lemon", "banana", "orange");
+
+foreach($fruits as $fruit){
+    echo "要素は" . $fruit;
     echo "\n";
 }
-/* for文の始めの値を定義する /
-
-/* for文の終わりの値を定義する */
 
 
-for($i = 1; $i <= 100; $i++){
+$start = 1;
+$end = 100; 
+
+
+for($i = $start; $i <= $end; $i++){
 
   // 5で割り切れたら{}内を実行する
-  if($i % 5 == 0) {
+  if($i % 5 == 0){
     echo $i;
-    echo "\n";
   }
-}  
+}
+
+ 
